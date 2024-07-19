73ed21631b0a6e0a4265760a645743e2c63b9ccf
Geovana-Becker <beckergeovanaa@gmail.com>
2024-07-19 12:47:01 -0300

    Trabalho utilizando API, tema: anime
    Códigos em Css, html

---
+++ Animes.cs
@@ -0,0 +1,11 @@
+﻿namespace animeApi
+{
+    public class Animes
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int Nota { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
